726b10f2b3f7356611f6970130a0396de8c38659
Borel KOUMO <borelkoumo@yahoo.fr>
2021-08-17 22:27:40 +0200

    36. Pqssing data via props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  const root = document.getElementById("root");
-  const para = document.createElement("p");
-  para.textContent = "THis is me";
-  root.append(para);
+  const expenses = [
+    {
+      id: 1,
+      title: "Car",
+      date: new Date(2021, 2, 28),
+      amount: 5000,
+    },
+    {
+      id: 2,
+      title: "Toilet paper",
+      date: new Date(2021, 2, 12),
+      amount: 100,
+    },
+    {
+      id: 3,
+      title: "Repairing house",
+      date: new Date(2021, 5, 5),
+      amount: 150000,
+    },
+  ];
+  let eList = [];
+  expenses.forEach((element) => {
+    eList.push(<ExpenseItem data={element}></ExpenseItem>);
+  });
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2>Monthly expenses</h2>
+      {eList}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,16 @@
-function ExpenseItem() {
-  return <h2>This is Expense Item</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const { id, title, date, amount } = props.data;
+
+  return (
+    <div className="expense-item">
+      <div>{date.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
